0e36a44eb70dced5ae7fd4fafb928b85d213b828
DeokJong <novelss444@jbnu.ac.kr>
2024-10-28 14:06:01 +0900

    etc: instruction file edit

--- instruction/24-02-WAS-assignment-02-angular-main/src/app/layout/header/header.component.ts
+++ instruction/24-02-WAS-assignment-02-angular-main/src/app/layout/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import {Router, RouterModule} from '@angular/router';
-import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { faSearch, faUser, faTicket, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import {CommonModule} from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Router, RouterModule } from '@angular/router'
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { faSearch, faUser, faTicket, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-header',
@@ -10,34 +10,36 @@ import {CommonModule} from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  isScrolled = false;
-  isMobileMenuOpen = false;
+  isScrolled = false
+  isMobileMenuOpen = false
 
-  constructor(private router: Router, library: FaIconLibrary) {
-    library.addIcons(faSearch, faUser, faTicket, faBars, faTimes);
+  constructor(
+    private router: Router,
+    library: FaIconLibrary
+  ) {
+    library.addIcons(faSearch, faUser, faTicket, faBars, faTimes)
   }
 
   ngOnInit() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('scroll', this.handleScroll)
   }
 
   ngOnDestroy() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('scroll', this.handleScroll)
   }
 
   removeKey() {
-    localStorage.removeItem('TMDb-Key');
-    this.router.navigate(['/signin']);
+    localStorage.removeItem('TMDb-Key')
+    this.router.navigate(['/signin'])
   }
 
   toggleMobileMenu() {
-    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    this.isMobileMenuOpen = !this.isMobileMenuOpen
   }
 
   handleScroll = () => {
-    this.isScrolled = window.scrollY > 50;
+    this.isScrolled = window.scrollY > 50
   }
 }
