586d96386453c49b5ec7e5a24e92868df34c77fc
DeokJong <novelss444@jbnu.ac.kr>
2024-10-29 11:49:21 +0900

    apply: theme.ts apply for default page layout

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -9,12 +9,39 @@ export const Route = createFileRoute('/_layout')({
 
 function Layout() {
   return (
-    <Box sx={{
-      background: 'black',
-      height: '100vh',
-    }}>
+    <Box
+      sx={{
+        background: ($theme) => $theme.palette.gradients.background,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Header />
-      <Outlet />
+      <Box
+        sx={{
+          flexGrow: 1,
+          marginTop: '80px',
+          paddingX: {
+            xs: 4, // 모바일에서 16px (2 * 8px)
+            sm: 6, // 작은 화면에서 32px
+            md: 8, // 중간 화면에서 64px
+            lg: 12, // 큰 화면에서 96px
+          },
+          paddingY: {
+            xs: 2, // 모바일에서 16px
+            sm: 3, // 작은 화면에서 24px
+            md: 4, // 중간 화면에서 32px
+            lg: 5, // 큰 화면에서 40px
+          },
+          overflow: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2, // Outlet 내부 요소 간 간격
+        }}
+      >
+        <Outlet />
+      </Box>
     </Box>
   )
 }
