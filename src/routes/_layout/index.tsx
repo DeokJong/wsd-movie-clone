f4cf39532f0b6649d08f347757e89eca5517d7ec
DeokJong <novelss444@jbnu.ac.kr>
2024-10-30 12:40:34 +0900

    etc: temp Discover API

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,13 +1,23 @@
+import { Button } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
+
+import { DiscoverService } from '@/Services'
 
 export const Route = createFileRoute('/_layout/')({
   component: index,
 })
 
 function index() {
+  const callAPI = async () => {
+    const response1 = await DiscoverService.Movie()
+    console.log(response1)
+    const response2 = await DiscoverService.TV()
+    console.log(response2)
+  }
+
   return (
     <>
-    Hello world!
+      <Button variant="contained" color="primary" onClick={callAPI} />
     </>
   )
 }
