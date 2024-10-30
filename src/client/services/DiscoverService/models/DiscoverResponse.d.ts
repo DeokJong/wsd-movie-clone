3b248de8afb9459dc932f9c7df183c75e4d010e6
DeokJong <novelss444@jbnu.ac.kr>
2024-10-30 15:42:53 +0900

    etc: Discover Respone type 분할

--- src/client/services/DiscoverService/models/DiscoverResponse.d.ts
+++ src/client/services/DiscoverService/models/DiscoverResponse.d.ts
@@ -1,29 +1,31 @@
+export type DiscoverResults = {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  overview: string
+  popularity: number
+  poster_path: string
+  vote_average: number
+  vote_count: number
+
+  // TV 전용 필드
+  origin_country?: string[]
+  original_name?: string
+  first_air_date?: string
+  name?: string
+
+  // Movie 전용 필드
+  original_title?: string
+  release_date?: string
+  title?: string
+  video?: boolean
+}
+
 export type DiscoverResponse = {
   page: number
-  results: {
-    adult: boolean
-    backdrop_path: string
-    genre_ids: number[]
-    id: number
-    original_language: string
-    overview: string
-    popularity: number
-    poster_path: string
-    vote_average: number
-    vote_count: number
-
-    // TV 전용 필드
-    origin_country?: string[]
-    original_name?: string
-    first_air_date?: string
-    name?: string
-
-    // Movie 전용 필드
-    original_title?: string
-    release_date?: string
-    title?: string
-    video?: boolean
-  }[]
+  results: DiscoverResults[]
   total_pages: number
   total_results: number
 }
