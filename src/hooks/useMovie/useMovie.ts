6e673c8deacf64e42237f1bc17289672a971d905
DeokJong <novelss444@jbnu.ac.kr>
2024-10-30 15:41:03 +0900

    feature: useMovie Data Query hook

---
+++ src/hooks/useMovie/useMovie.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+
+import { DiscoverResults, DiscoverService } from '@/Services'
+
+export const useMovie = () => {
+  const [page, setPage] = useState(1)
+
+  const { data, error, isLoading } = useQuery<DiscoverResults[], Error>({
+    queryKey: ['discover', 'movie', page],
+    queryFn: async () => (await DiscoverService.Movie({
+      query: {
+        page,
+      }
+    })).results
+  })
+
+  const handlePageChange = (page: number) => {
+    setPage(page)
+  }
+
+  return { data, error, isLoading, handlePageChange }
+}
