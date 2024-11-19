84c0728a3e4786dc62d6f911cc4273cabb922579
DeokJong <novelss444@jbnu.ac.kr>
2024-11-19 12:05:07 +0900

    feature: wisilist에 auth hook 적용

--- src/routes/_layout/wishlist.tsx
+++ src/routes/_layout/wishlist.tsx
@@ -1,5 +1,49 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+
+import { isAuth, useUserData } from '@/Hooks'
+import { ErrorModal } from '@/Components'
 
 export const Route = createFileRoute('/_layout/wishlist')({
-  component: () => <div>Hello /_layout/wishlist!</div>
+  component: wishlist,
+  // Remove beforeLoad since we're handling auth in the component
 })
+
+function wishlist() {
+  const { wishList } = useUserData()
+  const [open, setOpen] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isAuth()) {
+      setOpen(true)
+    }
+  }, [])
+
+  const handleClose = () => {
+    setOpen(false)
+    router.navigate({
+      to: '/signin'
+    })
+  }
+
+  return (
+    <>
+      <ErrorModal
+        open={open}
+        errorMessage="You must be logged in to view the wishlist."
+        onClose={handleClose}
+      />
+      { isAuth() && (
+        <div>
+          <h1>Wishlist</h1>
+          <ul>
+            {wishList.map((item) => (
+              <li key={item.id}>{item.timeStamps}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  )
+}
