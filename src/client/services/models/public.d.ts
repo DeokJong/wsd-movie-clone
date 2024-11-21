0d31afcd06aa565fb7626cfc3200c1c5c1ac077b
DeokJong <novelss444@jbnu.ac.kr>
2024-11-21 17:59:08 +0900

    feature: PublicFeatureDetail type

--- src/client/services/models/public.d.ts
+++ src/client/services/models/public.d.ts
@@ -29,7 +29,7 @@ export type FeatureResults = {
   profile_path?: string
 }
 
-export type DateRange = {
+type DateRange = {
   maximum: string
   minimum: string
 }
@@ -40,4 +40,105 @@ export type PublicFeature = {
   total_pages: number
   total_results: number
   dates?: DateRange // dates 필드 추가
+}
+
+type Creator = {
+  id: number
+  credit_id: string
+  name: string
+  original_name: string
+  gender: number
+  profile_path: string | null
+}
+
+type Genre = {
+  id: number
+  name: string
+}
+
+type Episode = {
+  id: number
+  name: string
+  overview: string
+  vote_average: number
+  vote_count: number
+  air_date: string
+  episode_number: number
+  episode_type: string
+  production_code: string
+  runtime: number | null
+  season_number: number
+  show_id: number
+  still_path: string | null
+}
+
+type Network = {
+  id: number
+  logo_path: string | null
+  name: string
+  origin_country: string
+}
+
+type ProductionCompany = {
+  id: number
+  logo_path: string | null
+  name: string
+  origin_country: string | null
+}
+
+type ProductionCountry = {
+  iso_3166_1: string
+  name: string
+}
+
+type Season = {
+  air_date: string
+  episode_count: number
+  id: number
+  name: string
+  overview: string
+  poster_path: string | null
+  season_number: number
+  vote_average: number
+}
+
+type SpokenLanguage = {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
+export type PublicFeatureDetail = {
+  adult: boolean
+  backdrop_path: string | null
+  created_by: Creator[]
+  episode_run_time: number[]
+  first_air_date: string
+  genres: Genre[]
+  homepage: string
+  id: number
+  in_production: boolean
+  languages: string[]
+  last_air_date: string
+  last_episode_to_air: Episode
+  name: string
+  next_episode_to_air: Episode
+  networks: Network[]
+  number_of_episodes: number
+  number_of_seasons: number
+  origin_country: string[]
+  original_language: string
+  original_name: string
+  overview: string
+  popularity: number
+  poster_path: string | null
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  seasons: Season[]
+  spoken_languages: SpokenLanguage[]
+  status: string
+  tagline: string
+  type: string
+  vote_average: number
+  vote_count: number
 }
