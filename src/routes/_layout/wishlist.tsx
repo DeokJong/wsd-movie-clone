4e2a9638d8d0a5d2ab8dc197c9a5b2470f9d282a
DeokJong <novelss444@jbnu.ac.kr>
2024-11-21 14:09:04 +0900

    feature: wishlist add beforeLoad

--- src/routes/_layout/wishlist.tsx
+++ src/routes/_layout/wishlist.tsx
@@ -1,41 +1,27 @@
-import { useEffect, useState } from 'react'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 import { isAuth, useUserData } from '@/Hooks'
-import { ErrorModal } from '@/Components'
 
 export const Route = createFileRoute('/_layout/wishlist')({
   component: wishlist,
-  // Remove beforeLoad since we're handling auth in the component
+  beforeLoad: () => {
+    if (!isAuth()) {
+      alert('You must be logged in to view the wishlist.')
+      throw redirect({ to: '/signin' })
+    }
+  },
 })
 
 function wishlist() {
   const { wishList } = useUserData()
-  const [open, setOpen] = useState(false)
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!isAuth()) {
-      setOpen(true)
-    }
-  }, [])
-
-  const handleClose = () => {
-    setOpen(false)
-    router.navigate({
-      to: '/signin'
-    })
-  }
-
   return (
     <>
-      <ErrorModal
-        open={open}
-        errorMessage="You must be logged in to view the wishlist."
-        onClose={handleClose}
-      />
-      { isAuth() && (
-        <div>
+      {wishList.length === 0
+        ? <div>
+          <h1>Wishlist</h1>
+          <p>Your wishlist is empty.</p>
+        </div>
+        : <div>
           <h1>Wishlist</h1>
           <ul>
             {wishList.map((item) => (
@@ -43,7 +29,7 @@ function wishlist() {
             ))}
           </ul>
         </div>
-      )}
+      }
     </>
   )
 }
