3f17aa25b0fc5e85ced31fefaa4111507562f325
DeokJong <novelss444@jbnu.ac.kr>
2024-11-25 17:12:50 +0900

    modify: 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -27,8 +27,9 @@ const handleRedirect = () => {
   const redirectPath = params.get('redirect')
   console.log('Current path:', window.location.pathname)
   console.log('redirectPath:', redirectPath)
-  if (redirectPath) {
-    router.navigate({ to: redirectPath }) // 라우터로 리다이렉트
+  if (redirectPath && redirectPath.startsWith('index.html/')) {
+    const newPath = redirectPath.replace('index.html/', '')
+    router.navigate({ to: newPath }) // 라우터로 리다이렉트
   }
 }
 
