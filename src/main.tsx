3ab88a676ff8f8bb4f775275d5ca9a7733524de2
DeokJong <novelss444@jbnu.ac.kr>
2024-11-25 17:31:18 +0900

    modify: 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -25,12 +25,11 @@ declare module '@tanstack/react-router' {
 const handleRedirect = () => {
   const params = new URLSearchParams(window.location.search)
   const redirectPath = params.get('redirect')
+  console.log('Current path:', window.location.pathname)
   console.log('redirectPath:', redirectPath)
-  if (redirectPath) {
-    router.navigate({
-      to: redirectPath,
-      replace: true,
-    })
+  if (redirectPath && redirectPath.startsWith('index.html/')) {
+    const newPath = redirectPath.replace('index.html/', '')
+    router.navigate({ to: `../${newPath}` }) // 라우터로 리다이렉트
   }
 }
 
