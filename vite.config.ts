29f65830041e99a8dd71680ed756bc607b6ab1bc
DeokJong <novelss444@jbnu.ac.kr>
2024-11-25 13:47:29 +0900

    conf: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import eslint from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  base: '/wsd-movie-clone/',
   build: {
-    outDir: './dist'
+    outDir: './dist',
   },
-  plugins: [TanStackRouterVite(), viteReact(), eslint(), tsconfigPaths()],
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    eslint(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html', // 프로젝트 루트에 위치한 404.html
+          dest: '.', // 빌드 디렉토리로 복사
+        },
+      ],
+    }),
+  ],
 })
